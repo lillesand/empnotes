53a6b2584e055e6747df67f1134029dd9b0341f0
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-12-25 23:25:56 +0100

    Seeding of employees

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem "twitter-bootstrap-rails"
 
 gem 'bootstrap-datepicker-rails'
 
+gem 'json'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+all_employees = JSON.parse(File.read("#{File.dirname(__FILE__)}/seeds/all.json"))
+
+
+number_of_inserts = 0
+employee_entities = []
+File.foreach("#{File.dirname(__FILE__)}/seeds/mine.txt", encoding: 'utf-8') { |line|
+  match = false
+  all_employees.each { |employee|
+    next if line == nil or employee == nil or employee['Name'] == nil
+    next if line.strip != employee['Name'].strip
+    match = true
+
+    next if Employee.exists? :bekk_id => employee['Id']
+    number_of_inserts = number_of_inserts + 1
+
+    Employee.new({ :bekk_id => employee['Id'], :name => employee['Name'] }).save
+  }
+
+  puts "No match for #{line}" unless match
+}
+
+puts "Inserted #{number_of_inserts} employees"
