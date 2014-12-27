2198aeec8496d6e2ebe24895e80cfd9106c15830
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-12-27 09:46:54 +0100

    Sort by time since last chat. Show less info in table

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,16 @@ class Employee < ActiveRecord::Base
   has_many :notes, -> { order('added_at desc') }
 
   def last_chat
-    notes
+    if notes.size > 0 then
+      notes[0].added_at
+    else
+      nil
+    end
+  end
+
+  def days_since_last_chat
+    return Float::INFINITY if last_chat == nil
+    (Time.now.to_date - last_chat.to_date).to_i
   end
 
 end
