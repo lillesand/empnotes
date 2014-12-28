7071f7ded5e2d5c500a07dadba4ead1390a8dba8
Jøran Vagnby Lillesand <lillesand@gmail.com>
2014-12-28 05:49:06 +0100

    Add most reent status in employee index

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,17 +2,21 @@ class Employee < ActiveRecord::Base
 
   has_many :notes, -> { order('added_at desc') }
 
-  def last_chat
-    if notes.size > 0 then
-      notes[0].added_at
-    else
-      nil
-    end
+  def last_note
+    return nil if notes.size < 1
+
+    notes[0]
   end
 
   def days_since_last_chat
-    return Float::INFINITY if last_chat == nil
-    (Time.now.to_date - last_chat.to_date).to_i
+    return Float::INFINITY if last_note == nil
+    (Time.now.to_date - last_note.added_at.to_date).to_i
+  end
+
+  def happiness
+    return 'Good' if last_note == nil
+
+    last_note.happiness
   end
 
 end
